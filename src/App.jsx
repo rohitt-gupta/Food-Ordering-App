7180eafc3de0c5300e262e2def512a48472b6ebd
rohitGupta2022 <pta.rohit28@gmail.com>
2023-04-01 21:52:51 +0530

    Added Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
+	const [cartIsShown, setCartIsShown] = useState(false);
+
+	const showCartHandler = () => {
+		setCartIsShown(true);
+		true;
+	};
+
+	const hideCartHandler = () => {
+		setCartIsShown(false);
+	};
+
 	return (
 		<>
-			<Header />
-			<Meals />
+			{cartIsShown && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
+			<main>
+				<Meals />
+			</main>
 		</>
 	);
 };
--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -5,7 +5,7 @@ const Input = (props) => {
 	return (
 		<div className={classes.input}>
 			<label htmlFor={props.input.id}>{props.label}</label>
-			<input type='text' id={props.input.id} />
+			<input {...props.input} />
 		</div>
 	);
 };
