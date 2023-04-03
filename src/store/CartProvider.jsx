bae8bd7cd96bad5fa70bacd132f1555151caef80
rohitGupta2022 <pta.rohit28@gmail.com>
2023-04-03 22:13:38 +0530

    FInal code added
    Bugs still remaining:=-
    if we increase an existing item in the cart, the amount will gets doubled. like if the amount is 4 it will become 8 etc..

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -10,20 +10,87 @@ import CartContext from "./cart-context";
 	 */
 }
 
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+/**
+ * We are using this useReducer insted of useState to manage the cart-context state.
+ * Purpose of this staAte management is to check if the item is already
+ * present in the cart then update the object otherwise add it to the cart.
+ * @param {*} state
+ * @param {*} action
+ * @returns
+ */
 const cartReducer = (state, action) => {
-	return;
+	if (action.type === "ADD") {
+		const updatedTotalAmount =
+			state.totalAmount + action.item.price * action.item.amount;
+
+		const existingCartItemsIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+		const existingCartItem = state.items[existingCartItemsIndex];
+		let updatedItems;
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemsIndex] = updatedItem;
+		} else {
+			updatedItems = state.items.concat(action.item);
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	if (action.type === "REMOVE") {
+		const existingCartItemsIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+		const existingItem = state.items[existingCartItemsIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemsIndex] = updatedItem;
+		}
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	return defaultCartState;
 };
 
 export default function CartProvider(props) {
-	const addItemToCartHandler = (item) => {};
+	const [cartState, dispatchCartAction] = useReducer(
+		cartReducer,
+		defaultCartState
+	);
 
-	const removeItemFromCarthandler = (id) => {};
+	const addItemToCartHandler = (item) => {
+		dispatchCartAction({ type: "ADD", item: item });
+	};
+
+	const removeItemFromCarthandler = (id) => {
+		dispatchCartAction({ type: "REMOVE", id: id });
+	};
 
 	const cartContext = {
 		//going to be dynamic data,or updates with time or
 		// updating with the provider type component.
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCarthandler,
 	};
